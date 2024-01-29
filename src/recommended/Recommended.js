2e11c378af5b971e2363cfe959536568ca12453f
abiodunraheem <raheemabiodun23@gmail.com>
2024-01-29 22:47:36 +0100

    Add recommended buttons

--- src/recommended/Recommended.js
+++ src/recommended/Recommended.js
@@ -2,7 +2,16 @@ import "./recommended.css";
 
 const Recommended = () => {
   return (
-    <div>Recommended</div>
+    <div>
+      <h2 className="recommended-title">Recommended</h2>
+      <div className="recommended-flex">
+        <button value="" title="All Products">All Products</button>
+        <button value="Nike" title="Nike">Nike</button>
+        <button value="Adidas" title="Adidas">Adidas</button>
+        <button value="Puma" title="Puma">Puma</button>
+        <button value="Vans" title="Vans">Vans</button>
+      </div>
+    </div>
   )
 }
 
